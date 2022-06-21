94138a6674c94682a985ef66f210d23443dab535
Andreas Runfalk <andreas@runfalk.se>
2022-06-21 22:12:46 +0200

    Fix auto complete hotkeys for latest nvim-cmp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -130,11 +130,34 @@ cmp.setup({
         end,
     },
     mapping = {
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
+        ['<Down>'] = cmp.mapping(
+            cmp.mapping.select_next_item({
+                behavior = cmp.SelectBehavior.Select,
+            }),
+            {'i'}
+        ),
+        ['<Up>'] = cmp.mapping(
+            cmp.mapping.select_prev_item({
+                behavior = cmp.SelectBehavior.Select,
+            }),
+            {'i'}
+        ),
+        ['<CR>'] = cmp.mapping({
+            i = cmp.mapping.confirm({
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = false,
+            }),
+            c = function(fallback)
+                if cmp.visible() then
+                    cmp.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = false,
+                    })
+                else
+                    fallback()
+                end
+            end
         }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
